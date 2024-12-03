8fbe39e51ffa9dd32b95ae9fc59ca0046e7f0794
AntonioNegreiros <121639185+AntonioNegreiros@users.noreply.github.com>
2024-12-03 20:41:56 -0300

    Add files via upload

---
+++ atividade_Java.sql
@@ -0,0 +1,15 @@
+use eventojoinville;
+show databases;
+show tables;
+describe evento;
+
+select * from evento;
+select * from participante;
+select * from organizador;
+select * from local;
+delete from evento where id = 20;
+
+
+describe participante;
+
+
